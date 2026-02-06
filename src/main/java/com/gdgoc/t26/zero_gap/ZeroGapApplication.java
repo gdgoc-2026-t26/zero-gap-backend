d78238e40b769735cb31dfe9ac7ccbd80798885e
ICubE- <jehoyeon01@gmail.com>
2026-02-06 20:31:45 +0900

    Setup Spring Boot

---
+++ src/main/java/com/gdgoc/t26/zero_gap/ZeroGapApplication.java
@@ -0,0 +1,13 @@
+package com.gdgoc.t26.zero_gap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZeroGapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZeroGapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gdgoc/t26/zero_gap/ZeroGapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gdgoc.t26.zero_gap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZeroGapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
